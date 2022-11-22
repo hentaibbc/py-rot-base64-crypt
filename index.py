7cc25c3f1db22185c41b2ed5f6847a074cd614b3
Tatsuya <tatsuya@dotech.com.tw>
2022-11-22 14:16:43 +0800

    update code style

--- index.py
+++ index.py
@@ -1,8 +1,19 @@
-from rot_base64_crypt.crypt import *
+from rot_base64_crypt.crypt import Crypt
+from string import Template
+
+privateKey='1qaz3edc5tgb6yhn4rfv2wsx'
+tpl = Template("""origin: $origin
+encrypt: $encrypt
+decrypt: $decrypt""")
 
 if __name__ == '__main__':
-    mycrypt = Crypt('123')
-    encoded = mycrypt.encrypt('123')
+    mycrypt = Crypt(privateKey)
+    origin = 'This is a test paragraph'
+    encoded = mycrypt.encrypt(origin)
     decoded = mycrypt.decrypt(encoded)
-    print(encoded)
-    print(decoded)
+
+    print(tpl.substitute(dict(
+        origin=origin,
+        encrypt=encoded,
+        decrypt=decoded,
+    )))
